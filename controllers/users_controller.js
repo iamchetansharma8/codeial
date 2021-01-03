2b4229830d9f087251338eddab9ea8addd24181a
Chetan <iamchetansharma8@gmail.com>
2021-01-03 13:02:08 +0530

    posts action added in users_ controller and used in users.js corresponding to /users/profile rout

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
 module.exports.profile=function(req,res){
     return res.end('<h1>Users</h1>');
 }
+module.exports.posts=function(req,res){
+    return res.end('<h1> User s posts');
+}
--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,8 @@ const usersController=require('../controllers/users_controller');
 
 router.get('/profile',usersController.profile);
 
+// directing to posts.js router for /users/posts
+
+router.get('/posts',usersController.posts);
+
 module.exports=router;
